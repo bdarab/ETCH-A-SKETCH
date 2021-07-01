0e0b175bbef0cc8e653e6458c9301c09c133c71f
bdarab <bdarabzand@gmail.com>
2021-07-02 00:37:17 +0200

    Add grids & button

---
+++ app.js
@@ -0,0 +1,17 @@
+const body = document.querySelector('body');
+const squares = document.querySelectorAll('.square');
+const Btn = document.createElement('button');
+const container = document.querySelector('.container');
+
+body.append(Btn);
+body.insertBefore(Btn, container);
+Btn.innerText = 'Create A New Grid';
+Btn.addEventListener('click', (e) => {
+	e.preventDefault();
+	for (i = 0; i < 256; i++) {
+		const el = document.createElement('div');
+
+		container.append(el);
+		let squares = el.classList.add('square');
+	}
+});
