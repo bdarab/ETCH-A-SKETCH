fa699cae85ca30f1262708bcc21b45af733aa403
bdarab <bdarabzand@gmail.com>
2021-07-02 12:42:02 +0200

    Final Commit

--- app.js
+++ app.js
@@ -1,18 +1,63 @@
-const body = document.querySelector('body');
-const squares = document.querySelectorAll('.square');
-const Btn = document.createElement('button');
+// window.addEventListener('DOMContentLoaded', (event) => {
+// 	console.log('DOM fully loaded and parsed');
+
+// 	const body = document.querySelector('body');
+// 	const Btn = document.createElement('button');
+// 	const container = document.querySelector('.container');
+// 	const squares = document.querySelectorAll('.square');
+// 	Btn.textContent = 'Create A New Grid';
+// 	body.insertBefore(Btn, container);
+
+// 	Btn.addEventListener('click', (e) => {
+// 		e.preventDefault();
+// 		for (let i = 0; i < 256; i++) {
+// 			const el = document.createElement('div');
+// 			container.append(el);
+// 			let squares = el.classList.add('square');
+// 			el.setAttribute('id', i);
+// 		}
+// 	});
+
+// 	squares.forEach((square) => {
+// 		square.addEventListener('mouseover', (e) => {
+// 			console.log(e);
+// 			e.preventDefault();
+// 			square.style.backgroundColor = 'black';
+// 		});
+// 	});
+// });
 const container = document.querySelector('.container');
 
-body.append(Btn);
-body.insertBefore(Btn, container);
-Btn.innerText = 'Create A New Grid';
-Btn.addEventListener('click', (e) => {
-	e.preventDefault();
-	for (i = 0; i < 256; i++) {
-		const el = document.createElement('div');
+const Btn = Object.assign(document.createElement('button'), {
+	textContent: `Click to Create A New Grid, 
+							Double Click to Reset`,
+});
+document.body.prepend(Btn);
 
-		container.append(el);
-		let squares = el.classList.add('square');
-		el.setAttribute('id', i);
-	}
+Btn.addEventListener(
+	'click',
+	(e) => {
+		for (let i = 0; i < 256; i++) {
+			const el = Object.assign(document.createElement('div'), {
+				className: 'square',
+				id: `per-${i}`,
+			});
+			container.append(el);
+		}
+	},
+	{ passive: true }
+);
+
+// Delegated event listener
+container.addEventListener(
+	'mouseover',
+	(e) => {
+		if (!e.target.matches('.container .square')) return;
+		e.target.classList.add('hovered');
+	},
+	{ passive: true }
+);
+
+Btn.addEventListener('dblclick', (e) => {
+	window.location.reload();
 });
